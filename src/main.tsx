ded9bf8a31b4d4ffcd410741162c54a0a4c901fc
streaker0 <mujeebakinbile@hotmail.com>
2025-01-10 21:05:02 -0500

    added amplify configure

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
