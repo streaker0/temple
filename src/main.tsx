81ecbeefd9284b925bfbf90abb078bd4bbeacdbe
streaker0 <mujeebakinbile@hotmail.com>
2025-01-12 03:22:33 -0500

    amplify changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { Amplify } from 'aws-amplify';
 import outputs from "../amplify_outputs.json";
-Amplify.configure({
-	Auth: {
-	  Cognito: {
-		userPoolId: outputs.auth.user_pool_id,
-		userPoolClientId: outputs.auth.user_pool_client_id,
-		identityPoolId: outputs.auth.identity_pool_id,
-		loginWith: {
-		  email: true,
-		  username: true,
-		  phone: false
-		}
-	  }
-	}
-  });
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
